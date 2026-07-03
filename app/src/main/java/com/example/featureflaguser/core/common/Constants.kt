688ea164b1313505c8eee330045868495920bdd8
Sandhra Santeev <sandhra25@gmail.com>
2026-07-03 20:44:15 +0530

    Configure Hilt Dependency Injection

---
+++ app/src/main/java/com/example/featureflaguser/core/common/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.featureflaguser.core.common
+
+object Constants {
+//    const val BASE_URL = "http://10.0.2.2:8080/"
+    const val BASE_URL = "http://192.168.29.117:4000/"
+}
---
+++ app/src/main/java/com/example/featureflaguser/core/network/NetworkModule.kt
@@ -0,0 +1,39 @@
+package com.example.featureflaguser.core.network
+
+import com.example.featureflaguser.core.common.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import javax.inject.Singleton
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)//Keep everything inside this module alive for the lifetime of this application.
+object NetworkModule{
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor():HttpLoggingInterceptor{
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+    @Provides
+    @Singleton//One okhttp for the entire app
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient):Retrofit{
+        return Retrofit.Builder().baseUrl(Constants.BASE_URL).client(okHttpClient).addConverterFactory(GsonConverterFactory.create()).build()
+    }
+
+
+}
